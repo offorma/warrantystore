4da7b41f8300d80da811a8ed4b32c61d30e5b63a
Chukwunonso <offorma@gmail.com>
2017-03-12 18:12:20 +0000

    i got here

--- php/login.php
+++ php/login.php
@@ -14,7 +14,7 @@ if (isset($_POST['login-btn'])) {
         <span class='glyphicon glyphicon-info-sign'></span>Email field cannot be empty </div>";
         header("Location: loginpg.php");
     }
-    if ((empty($_POST['password']))&&($_POST['password']!="")) {//this checks if password field is empty
+    if ((empty($_POST['password']))||($_POST['password']!="")) {//this checks if password field is empty
         $_SESSION['loginmessage'] = "<div class='alert alert-danger'>
      <span class='glyphicon glyphicon-info-sign'></span> Password field cannot be empty </div>";
         header("Location: loginpg.php");
