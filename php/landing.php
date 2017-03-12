adc58373115165acbebf4e0a218e274162cf39fc
Chukwunonso <offorma@gmail.com>
2017-03-12 23:32:08 +0000

    i got here

--- php/landing.php
+++ php/landing.php
@@ -7,14 +7,15 @@
  */
 session_start();
 include('header.php');?>
-
-<div class="col-md-6 col-md-offset-3">
-    <label class="fileContainer dot">
-        Click here to trigger the file uploader!
-        <input type="file"/>
-    </label>
-    
- </div>
+<body>
+    <div class="container">
+    <div class="col-md-6 col-md-offset-3">
+        <label class="fileContainer dot">
+            Click here to trigger the file uploader!
+            <input type="file"/>
+        </label>
+    </div>
+     </div>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
