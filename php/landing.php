58576ab23e42d00b5a3b17a9d1aa846dd887dbcb
Chukwunonso <offorma@gmail.com>
2017-03-12 23:12:54 +0000

    i got here

--- php/landing.php
+++ php/landing.php
@@ -6,10 +6,17 @@
  * Time: 5:33 PM
  */
 session_start();
-include('header.php');
-echo '<label class="btn btn-default btn-file">
+include('header.php');?>
+
+<div class="col-md-6 col-md-offset-3">
+<label class="btn btn-default btn-file">
     Browse <input type="file" hidden>
 </label>
     
- </div>'?>
+ </div>
 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
