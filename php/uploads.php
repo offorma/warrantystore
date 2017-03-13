4b59eb387c2331c3245c78b14c322e33b1a38674
Chukwunonso <offorma@gmail.com>
2017-03-13 23:58:48 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -9,7 +9,7 @@ session_start();
 
     if(isset($_POST["file-btn"])) {
 
-        $folder="/php/uploads/";
+        $folder="http://teamewarranty.azurewebsites.net/php/uploads/";
         echo print_r($_FILES['image']);
         $imgFile = $_FILES['image']['name'];
         $tmp_dir = $_FILES['image']['tmp_name'];
