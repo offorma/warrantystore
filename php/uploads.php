77bb54cc1c615c67f4435e71d3c340d8d00036f5
Chukwunonso <offorma@gmail.com>
2017-03-13 13:34:25 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -10,7 +10,7 @@ session_start();
     if(isset($_POST["file-btn"])) {
 
         $folder="uploads/";
-        if(!empty($_FILES['image'])) {
+        if(isset($_FILES['image'])) {
             $pic = rand(1000,100000)."-".$_FILES['image']['name'];
             $pic_loc = $_FILES['pic']['tmp_name'];
 
