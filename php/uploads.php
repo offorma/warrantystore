9c5d404fd6811cbf2f10c8ea23b5ca61b2cbc301
Chukwunonso <offorma@gmail.com>
2017-03-13 16:16:30 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -17,16 +17,16 @@ session_start();
         if($imgFile) {
 
              //generate random image name
-            $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension
+            $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension and make it lowercase
             $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
 
             $pic = rand(1000,100000000)."-".$imgFile;
 
-            if(in_array($imgExt, $valid_extensions)){
+            if(in_array($imgExt, $valid_extensions)){//check if extension is valid
 
                 switch ($imgExt) {
                     case "gif ":
-                        if (($imgSize > 5242880) && ($_FILES['image']['error'] == 0)) {
+                        if (($imgSize < 5242880) && ($_FILES['image']['error'] == 0)) {
                             $_SESSION['imgsize'] = "Image size must be less than 5MB";
                         } else {
                             move_uploaded_file($tmp_dir, $folder . $pic);
@@ -35,7 +35,7 @@ session_start();
                         echo "Image is a gif";
                         break;
                     case "jpeg":
-                        if (($imgSize > 5242880) && ($_FILES['image']['error'] == 0)) {
+                        if ($imgSize > 5242880) {
                             $_SESSION['imgsize'] = "Image size must be less than 5MB";
                         } else {
                             move_uploaded_file($tmp_dir, $folder . $pic);
@@ -44,7 +44,7 @@ session_start();
                         echo "Image is a jpeg";
                         break;
                     case "png ":
-                        if (($imgSize > 5242880) && ($_FILES['image']['error'] == 0)) {
+                        if ($imgSize > 5242880) {
                             $_SESSION['imgsize'] = "Image size must be less than 5MB";
                         } else {
                             move_uploaded_file($tmp_dir, $folder . $pic);
@@ -53,7 +53,7 @@ session_start();
                         echo "Image is a png";
                         break;
                     case "jpg ":
-                        if (($imgSize > 5242880) && ($_FILES['image']['error'] == 0)) {
+                        if ($imgSize > 5242880) {
                             $_SESSION['imgsize'] = "Image size must be less than 5MB";
                         } else {
                             move_uploaded_file($tmp_dir, $folder . $pic);
