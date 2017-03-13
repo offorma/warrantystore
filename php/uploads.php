9a4f81ffcb3c63e8df52c928cbcc7a1c29b77bcf
Chukwunonso <offorma@gmail.com>
2017-03-13 12:24:40 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -5,20 +5,52 @@
  * Date: 3/13/17
  * Time: 11:18 AM
  */
-
+session_start();
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 // Check if image file is a actual image or fake image
-if(isset($_POST["file-btn"])) {
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
+    if(isset($_POST["file-btn"])) {
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check !== false) {
+
+                switch ($check["mime"]) {
+                    case "image/gif":
+                        if($_FILES['image']['tmp_name']>5242880){
+                            $_SESSION['imgsize']="Image size must be less than 5MB";
+                        }else{
+                            move_uploaded_file($_FILES["image"]["tmp_name"], $target);
+                        }
+                        echo "Image is a gif";
+                        break;
+                    case "image/jpeg":
+                        if($_FILES['image']['tmp_name']>5242880){
+                            $_SESSION['imgsize']="Image size must be less than 5MB";
+                        }else{
+                            move_uploaded_file($_FILES["image"]["tmp_name"], $target);
+                        }
+                        echo "Image is a jpeg";
+                        break;
+                    case "image/png":
+                        if($_FILES['image']['tmp_name']>5242880){
+                            $_SESSION['imgsize']="Image size must be less than 5MB";
+                        }else{
+                            move_uploaded_file($_FILES["image"]["tmp_name"], $target);
+                        }
+                        echo "Image is a png";
+                        break;
+                    case "image/bmp":
+                        if($_FILES['image']['tmp_name']>5242880){
+                            $_SESSION['imgsize']="Image size must be less than 5MB";
+                        }else{
+                            move_uploaded_file($_FILES["image"]["tmp_name"], $target);
+                        }
+                        echo "Image is a bmp";
+                        break;
+                }
+        } else {
         echo "File is not an image.";
         $uploadOk = 0;
+        }
     }
-}
 ?>
