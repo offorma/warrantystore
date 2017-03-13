648d48bcac80ea4a54479e11604e9f3545683e9a
Chukwunonso <offorma@gmail.com>
2017-03-13 21:40:32 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -23,65 +23,66 @@ session_start();
 
             echo "this image extension".($imgExt)."<br>";
             $pic = rand(1000,100000000)."-".$imgFile;
-            echo "this the generated name".($pic)."<br>";}
+            echo "this the generated name".($pic)."<br>";
 
         echo in_array($imgExt, $valid_extensions);
-            /*
+
             if(in_array($imgExt, $valid_extensions)){//check if extension is valid
 
-                switch ($imgExt) {
-                    case "gif ":
-                        if (($imgSize < 5242880) && ($_FILES['image']['error'] == 0)) {
-                            $_SESSION['imgsize'] = "Image size must be less than 5MB";
-                        } else {
-                            if(move_uploaded_file($tmp_dir, $folder . $pic)){
-                                header('location: landing.php');
-                            }else{
-                                echo "Image cannot be moved";
-                            }
-                        }
-                        echo "Image is a gif";
-                        break;
-                    case "jpeg":
-                        if ($imgSize > 5242880) {
-                            $_SESSION['imgsize'] = "Image size must be less than 5MB";
-                        } else {
-                            if(move_uploaded_file($tmp_dir, $folder . $pic)){
-                                header('location: landing.php');
-                            }else{
-                                echo "Image cannot be moved";
-                            }
-                        }
-                        echo "Image is a jpeg";
-                        break;
-                    case "png ":
-                        if ($imgSize > 5242880) {
-                            $_SESSION['imgsize'] = "Image size must be less than 5MB";
-                        } else {
-                            if(move_uploaded_file($tmp_dir, $folder . $pic)){
-                                header('location: landing.php');
-                            }else{
-                                echo "Image cannot be moved";
-                            }
-                        }
-                        echo "Image is a png";
-                        break;
-                    case "jpg ":
-                        if ($imgSize > 5242880) {
-                            $_SESSION['imgsize'] = "Image size must be less than 5MB";
-                        } else {
-                            if(move_uploaded_file($tmp_dir, $folder . $pic)){
-                            header('location: landing.php');
-                            }else{
-                                echo "Image cannot be moved";
-                            }
-                        }
-                        echo "Image is a jpg";
-                        break;
-                }
-            }
-        }
-        else {
-            echo "File is not an image.";
-        }*/
+                      switch ($imgExt) {
+                           case "gif ":
+                               if (($imgSize < 5242880) && ($_FILES['image']['error'] == 0)) {
+                                   $_SESSION['imgsize'] = "Image size must be less than 5MB";
+                                   echo $_SESSION['imgsize'];
+                               } else {
+                                   if(move_uploaded_file($tmp_dir, $folder . $pic)){
+                                       header('location: landing.php');
+                                   }else{
+                                       echo "Image cannot be moved";
+                                   }
+                               }
+                               echo "Image is a gif";
+                               break;
+                           case "jpeg":
+                               if ($imgSize > 5242880) {
+                                   $_SESSION['imgsize'] = "Image size must be less than 5MB";
+                               } else {
+                                   if(move_uploaded_file($tmp_dir, $folder . $pic)){
+                                       header('location: landing.php');
+                                   }else{
+                                       echo "Image cannot be moved";
+                                   }
+                               }
+                               echo "Image is a jpeg";
+                               break;
+                           case "png ":
+                               if ($imgSize > 5242880) {
+                                   $_SESSION['imgsize'] = "Image size must be less than 5MB";
+                               } else {
+                                   if(move_uploaded_file($tmp_dir, $folder . $pic)){
+                                       header('location: landing.php');
+                                   }else{
+                                       echo "Image cannot be moved";
+                                   }
+                               }
+                               echo "Image is a png";
+                               break;
+                           case "jpg ":
+                               if ($imgSize > 5242880) {
+                                   $_SESSION['imgsize'] = "Image size must be less than 5MB";
+                               } else {
+                                   if(move_uploaded_file($tmp_dir, $folder . $pic)){
+                                   header('location: landing.php');
+                                   }else{
+                                       echo "Image cannot be moved";
+                                   }
+                               }
+                               echo "Image is a jpg";
+                               break;
+                       }
+                   }
+               }
+               else {
+                   echo "File is not an image.";
+               }
     }
