5e8a057526ad54e75027c64f5a879fc819aa7a30
Chukwunonso <offorma@gmail.com>
2017-03-13 09:41:40 +0000

    i got here

--- php/landing.php
+++ php/landing.php
@@ -11,7 +11,7 @@ include('header.php');?>
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3">
-                <form class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3">
+                <form class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
                     <div class="form-group ">
                         <input type="file" name="file" id="file" class="inputfile" />
                         <label class="center-block" for="file">Choose an image to upload</label>
