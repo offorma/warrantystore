69b32c28f75756f089d50aa95fb2366f29b27d90
Chukwunonso <offorma@gmail.com>
2017-03-13 10:54:01 +0000

    i got here

--- php/upload.php
+++ php/upload.php
@@ -5,33 +5,37 @@
  * Date: 3/13/17
  * Time: 10:07 AM
  */
-
+ session_start();
    if(isset($_FILES['image'])){
        $rnd=rand(100,999);
        $rnd=$rnd."_";
-       $errors= array();
+
        $file_name = $rnd.$_FILES['image']['name'];
        $file_size =$_FILES['image']['size'];
        $file_tmp =$_FILES['image']['tmp_name'];
        $file_type=$_FILES['image']['type'];
-       $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+       $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));//get file extension
 
        $expensions= array("jpeg","jpg","png","gif");
 
        if(in_array($file_ext,$expensions)=== false){
-           $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+           $errors= true;
+               $_SESSION['imgext']="extension not allowed, please choose a JPEG or PNG file.";
+               header('location: landing.php');
        }
 
        if($file_size > 5242880){
-           $errors[]='File size must be excately 5 MB';
+           $errors= true;
+           $_SESSION['imgsize']='File size cannot exceed 5 MB';
+           header('location: landing.php');
        }
 
-       if(empty($errors)==true){
+       if($errors!=true){
 
            move_uploaded_file($file_tmp,"uploads/".$file_name);
-           echo "Success";
-       }else{
-           print_r($errors);
+          $_SESSION['imgsuccess']='Image upload was sucessful';
+           header('location: landing.php');
        }
+
    }
 ?>
