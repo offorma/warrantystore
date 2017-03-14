f4a85f6d9e3cc17509cbb6e4f8a0500f42a46c50
Chukwunonso <offorma@gmail.com>
2017-03-14 00:56:31 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -9,7 +9,7 @@ session_start();
 
     if(isset($_POST["file-btn"])) {
 
-        $folder="/uploads";
+        $folder="../wwwroot";
         echo print_r($_FILES['image']);
         $imgFile = $_FILES['image']['name'];
         $tmp_dir = $_FILES['image']['tmp_name'];
