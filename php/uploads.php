a065823d30092110db437cc92694b94520391d31
Chukwunonso <offorma@gmail.com>
2017-03-14 18:23:51 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -54,7 +54,7 @@ require_once 'db.php';
                                        $userid = $urow['userid'];
 
                                        move_uploaded_file($tmp_dir, $folder . $pic);
-                                        $imgurl = $folder . $pic;
+                                       $imgurl = $folder . $pic;
                                        $conn->query("INSERT INTO receipt (image_url, receipt_number, total_charge, categoryid, userid)VALUES ('$imgurl', '$rnumber1', '$tcharge1','$categoryid','$userid')");
                                        $conn->close();
                                        header('location: landing.php');
