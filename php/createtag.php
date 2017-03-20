b0618beaf1668fd00a3e4d1a01ebeb12488e3e86
Chukwunonso <offorma@gmail.com>
2017-03-20 21:34:59 +0000

    i got here

---
+++ php/createtag.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: offormachukwunonso
+ * Date: 3/20/17
+ * Time: 8:58 PM
+ */
+session_start();
+require_once 'db.php';
+if(isset($_POST["createtag-btn"])) {
+    if(isset($_POST["tag"])&&(!empty(tag))){
+
+        $tag = $conn->real_escape_string(strip_tags($_POST['tag']));
+        $usersession = $_SESSION['userSession'];
+
+        $user = $conn->query("SELECT userid FROM user WHERE username='$usersession'");
+        $urow = $user->fetch_assoc();
+        $userid = $urow['userid'];
+
+        $check_tag = $conn->query("SELECT name FROM tag WHERE name like '%$tag%'");
+        $count=$check_tag->num_rows;
+
+        if ($count==0) {
+
+            $query = "INSERT INTO tag(name,userid) VALUES('$tag','$userid')";
+
+            if ($conn->query($query)) {
+                $_SESSION['msgstag'] = "<div class='alert alert-success'>
+              <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Tag successfully created!
+             </div>";
+                header("Location: landing.php");
+            } else {
+
+                $_SESSION['tagsqlmsg'] = "<div class='alert alert-danger'>
+              <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Error while creating tag !
+             </div>";
+                header("Location: landing.php");
+            }
+        }
+    }else {
+        $_SESSION['tagreqmsg'] = "<div class='alert alert-danger'>
+              <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Tag name is required!
+             </div>";
+        header("Location: landing.php");
+    }
+
+
+}
--- php/landing.php
+++ php/landing.php
@@ -14,33 +14,54 @@ if(!isset($_SESSION['userSession'])){
         <body>
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 image-form">
+                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-6>
                         <?php $pageWasRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0';
-                        if($pageWasRefreshed){
-                            unset($_SESSION['fileextention']);
-                            unset($_SESSION['imgsize']);
-                            unset($_SESSION['fileerror']);
-                            unset($_SESSION['filesuccess']);
-                            unset($_SESSION['emptyInput']);
-                        }
-                        ?>
+                    if($pageWasRefreshed){
+                        unset($_SESSION['fileextention']);
+                        unset($_SESSION['imgsize']);
+                        unset($_SESSION['fileerror']);
+                        unset($_SESSION['filesuccess']);
+                        unset($_SESSION['emptyInput']);
+                        unset($_SESSION['msgstag']);
+                        unset($_SESSION['tagsqlmsg']);
+                        unset($_SESSION['tagreqmsg']);
+                    }
+                    ?>
                         <?php if (isset($_SESSION['fileextention'])){
-                            echo $_SESSION['fileextention'];
-                        } ?>
+                        echo $_SESSION['fileextention'];
+                    } ?>
                         <?php if (isset($_SESSION['imgsize'])){
-                            echo $_SESSION['imgsize'];
-                        } ?>
+                        echo $_SESSION['imgsize'];
+                    } ?>
                         <?php if (isset($_SESSION['fileerror'])){
-                            echo $_SESSION['fileerror'];
-                        } ?>
+                        echo $_SESSION['fileerror'];
+                    } ?>
                         <?php
-                        if (isset($_SESSION['filesuccess'])){
-                            echo $_SESSION['filesuccess'];
-                        } ?>
+                    if (isset($_SESSION['filesuccess'])){
+                        echo $_SESSION['filesuccess'];
+                    } ?>
                         <?php
-                        if (isset($_SESSION['emptyInput'])){
-                            echo $_SESSION['emptyInput'];
-                        } ?>
+                    if (isset($_SESSION['emptyInput'])){
+                        echo $_SESSION['emptyInput'];
+                    } ?>
+                         <?php if (isset($_SESSION['msgstag'])){
+                        echo $_SESSION['msgstag'];
+                    } ?>
+                        <?php
+                    if (isset($_SESSION['tagsqlmsg'])){
+                        echo $_SESSION['tagsqlmsg'];
+                    } ?>
+                        <?php
+                    if (isset($_SESSION['tagreqmsg'])){
+                        echo $_SESSION['tagreqmsg'];
+                    } ?>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 image-form">
+
 
                         <form class=" form-img center-block " action="uploads.php" method="post" enctype="multipart/form-data">
                             <div class="form-group center-block">
@@ -60,8 +81,13 @@ if(!isset($_SESSION['userSession'])){
                             <div class="form-group">
                                 <label for="category">Category</label>
                                 <?php
+                                $usersession = $_SESSION['userSession'];
 
-                                $sql = "SELECT name FROM category";
+                                $user = $conn->query("SELECT userid FROM user WHERE username='$usersession'");
+                                $urow = $user->fetch_assoc();
+                                $userid = $urow['userid'];
+
+                                $sql = "SELECT name FROM category where userid = '$userid'";
                                 $result = $conn->query($sql);?>
                                 <select class="selectpicker form-control" name="category">
                                 <?php
@@ -81,12 +107,12 @@ if(!isset($_SESSION['userSession'])){
                     </div>
                     <div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1">
                         <div>
-                            <form  class="form-img" action="">
+                            <form  class="form-img" action="createtag.php" method="post">
                                 <div class="form-group">
                                     <label for="tag">Tag Name</label>
                                     <input type="text" class="form-control" id="tag" name="tag" placeholder="Enter a tag name">
                                 </div>
-                                <button type="submit" name ="file-btn" class="btn btn-default center-block">Create Tag</button>
+                                <button type="submit" name ="createtag-btn" class="btn btn-default center-block">Create Tag</button>
                             </form>
 
                         </div>
