4cb112a8aa410d34feb9153b22996d3388c6fd58
Chukwunonso <offorma@gmail.com>
2017-03-21 09:00:46 +0000

    i got here

--- php/uploads.php
+++ php/uploads.php
@@ -59,11 +59,9 @@ require_once 'db.php';
                                        $imgurl = $folder . $pic;
                                        $conn->query("INSERT INTO receipt (image_url, receipt_number, total_charge, categoryid, userid, details)VALUES ('$imgurl', '$rnumber1', '$tcharge1','$categoryid','$userid','')");
 
-                                       $rid = mysqli_insert_id();
+                                       echo $rid = mysqli_insert_id();
 
 
-                                       $_SESSION['fileerror'] = "<div class='alert alert-danger'>
-                                       <span class='glyphicon glyphicon-info-sign'></span> echo($rid) Image cannot be moved</div>";
 
                                            foreach ($tag as $select) {
                                                $conn->query(" INSERT INTO receipt_tag(receiptid,tagid)VALUES('$rid','$select')");
