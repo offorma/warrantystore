2ac25135d77cea314475d9570dfa8d9b79ecad6a
Chukwunonso <offorma@gmail.com>
2017-03-21 19:03:32 +0000

    i got here

--- php/gallery.php
+++ php/gallery.php
@@ -31,7 +31,7 @@ require_once 'db.php';
                 $imageThumbURL = $row["image_url"];
                 $imageURL = $row["image_url"];
                 ?>
-                <div class="img-thumbnail col-md-4 col-lg-4 col-xs-12><a href="<?php echo $imageURL; ?>" data-fancybox="group" data-caption="<?php ?>" >
+                <div class="col-md-4 col-lg-4 col-xs-12><a href="<?php echo $imageURL; ?>" data-fancybox="group" data-caption="<?php ?>" >
                     <img  class="img-thumbnail col-md-4 col-lg-4 col-xs-12" width="100px" height="100px" src="<?php echo $imageThumbURL; ?>" alt="" />
                 </a></div>
                 <?php
