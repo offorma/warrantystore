86f057779ae405cee7e9bb981561c9dbe33b188a
a <a@a.com>
2017-03-30 13:43:47 +0100

    connection check

--- php/search.php
+++ php/search.php
@@ -1,7 +1,55 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1516734
- * Date: 30/03/2017
- * Time: 12:21
- */
+
+session_start();
+
+?>
+
+
+<div>
+                            <form class="search" action="search.php" method="post">
+
+                                    <label for="search">Search by Tag Name</label>
+                                    <input type="text" class="form-control" id="search" name="tag" placeholder="Enter a Tag name">
+
+                                <button type="submit" name ="search-btn" class="btn btn-default center-block"> Search </button>
+                            </form>
+                        </div>
+
+<?
+include 'db.php';
+
+
+
+
+
+
+// search by tag name
+
+
+
+
+if(isset($_POST["tag"])) {
+
+    $tag = $_POST["tag"];
+    echo $tag;
+
+    $get_tagid = $conn->query("SELECT tagid FROM tag WHERE name like '%$tag%'");
+
+    $urow = $get_tagid->fetch_assoc();
+    $tagid = $urow['tagid'];
+
+    $get_receiptid = $conn->query("SELECT receiptid FROM receipt_tag WHERE tagid= '$tagid'");
+
+    $receiptid = array();
+
+    while($urow = $get_receiptid->fetch_assoc()){
+
+        $receiptid[] = $urow;
+    }
+
+echo $receiptid;
+
+
+
+
+}
