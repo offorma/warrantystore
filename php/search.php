8f155e4294e42dea010d7844e253123e861df6c0
a <a@a.com>
2017-03-30 13:55:08 +0100

    search

--- php/search.php
+++ php/search.php
@@ -43,7 +43,7 @@ if(isset($_POST["tag"])) {
 
     $receiptid = array();
 
-    while($urow = $conn->fetch_assoc($get_receiptid)){
+    while($urow = $conn->mysqli_fetch_assoc($get_receiptid)){
 
         $receiptid[] = $urow;
     }
