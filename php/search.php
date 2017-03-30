1a50bb3f8274912f1afbe8dd204671e5e134de2a
a <a@a.com>
2017-03-30 16:29:31 +0100

    search

--- php/search.php
+++ php/search.php
@@ -56,8 +56,8 @@ if(isset($_POST["tag"])) {
 //get image
         $get_img = $conn->query("SELECT image_url FROM receipt WHERE receiptid= '$receiptid'");
         echo $get_img;
-     //   $urow = $get_receiptimg->fetch_assoc();
-       // $image_url = $urow['image_url'];
+       $urow = $get_receiptimg->fetch_assoc();
+        $image_url = $urow['image_url'];
 
 
 
