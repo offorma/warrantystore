8646e5c494f9bb5ca0e445b32d0847d18700251e
Chukwunonso <offorma@gmail.com>
2017-04-03 18:08:00 +0100

    adding form to gallery

--- php/gallery.php
+++ php/gallery.php
@@ -117,6 +117,7 @@ require_once 'db.php';
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
 <script src='https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js'></script>
