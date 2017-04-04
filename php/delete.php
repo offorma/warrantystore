f010937dd4f8c1af70ee7fd0e1f7ab8cab0fbbcc
Chukwunonso <offorma@gmail.com>
2017-04-04 02:31:23 +0100

    adding form to gallery

--- php/delete.php
+++ php/delete.php
@@ -7,9 +7,29 @@
  */
 session_start();
 require_once 'db.php';
-$url=$_POST['url'];
-echo $url;
+if(!isset($_SESSION['userSession'])){
+    header('location:index.php');
+
+}
 
 if (isset($_POST['btn-ok'])) {
-    echo "hdh";
+    $file = $_POST['url'];
+    if (!unlink($file))
+    {
+        $_SESSION['filemsg'] = "<div class='alert alert-danger'>
+                <span class='glyphicon glyphicon-info-sign'></span> Oops!! Your file could not be deleted </div>";
+        header('location:gallery.php');
+    }
+    else{
+        $usersession = $_SESSION['userSession'];
+        $user = $conn->query("SELECT userid FROM user WHERE username='$usersession'");
+        $urow = $user->fetch_assoc();
+        $userid = $urow['userid'];
+
+        $query = $conn->query("DELETE * FROM receipt where userid = '$userid'AND image_url='$file'");
+
+        $_SESSION['filemsg'] = "<div class='alert alert-danger'>
+                <span class='glyphicon glyphicon-info-sign'></span> Your file was successfully deleted</div>";
+        header('location:gallery.php');
+    }
 }
