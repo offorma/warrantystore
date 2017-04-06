51b855b152650e606dee29d5fb5d842faadaf9a8
Chukwunonso <offorma@gmail.com>
2017-04-06 13:25:42 +0100

    adding form to gallery

--- php/login.php
+++ php/login.php
@@ -9,12 +9,12 @@ session_start();
 require_once 'db.php';
 
 if (isset($_POST['login-btn'])) {
-    if ((empty($_POST['email']))||($_POST['email']!="")) {// this checks if email field is empty
+    if ((empty($_POST['email']))||($_POST['email']=="")) {// this checks if email field is empty
         $_SESSION['emailmessage'] = "<div class='alert alert-danger'>
         <span class='glyphicon glyphicon-info-sign'></span>Email field cannot be empty </div>";
         header('Location: loginpg.php');
     }
-    if ((empty($_POST['password']))||($_POST['password']!="")) {//this checks if password field is empty
+    if ((empty($_POST['password']))Z($_POST['password']=="")) {//this checks if password field is empty
         $_SESSION['passwordmessage'] = "<div class='alert alert-danger'>
      <span class='glyphicon glyphicon-info-sign'></span> Password field cannot be empty </div>";
         header('Location:loginpg.php');
