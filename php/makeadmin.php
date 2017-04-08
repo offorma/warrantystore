b7e9343303d2c77f10007b975df9ee2cee578ad6
Chukwunonso <offorma@gmail.com>
2017-04-08 03:16:33 +0100

    adding form to gallery

--- php/makeadmin.php
+++ php/makeadmin.php
@@ -30,20 +30,10 @@ if(isset($_POST["make"])) {
 
 if(isset($_POST['uma'])) {
         $uid = $_POST['uid'];
-        $user = $conn->query("SELECT * FROM user WHERE userid='$uid'");
-        $urow = $user->fetch_assoc();
-        $userid = $urow['userid'];
-        $admin=$urow['admin'];
-        $active = $urow['active'];
-        $password = $urow['password'];
-        $hash=$urow['hash'];
-        $username=$urow['username'];
-        $email=$urow['email'];
 
 
-        $checks=$conn->query("UPDATE user SET admin = 0,active = $active,hash = '$hash',username = '$username'
-            ,email= '$email',password = '$password',userid = '$userid'WHERE `userid` = '$userid'");
-            if ($checks) {
+
+    if ($conn->query("UPDATE user SET admin=0 WHERE userid='$uid'")) {
             $_SESSION['verifymsg'] = "<div class='alert alert-success'>
             <span class='glyphicon glyphicon-info-sign'></span>Admin privilege has been revoked!
             </div>";
