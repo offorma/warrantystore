eb3f8ee5d3388fd73c51b5d484a83f1e99e1efe2
Chukwunonso <offorma@gmail.com>
2017-04-08 03:12:32 +0100

    adding form to gallery

--- php/makeadmin.php
+++ php/makeadmin.php
@@ -14,20 +14,21 @@ if(!isset($_SESSION['userSession'])||$_SESSION['active'] == false){
 if(isset($_POST["make"])) {
     $id = $_POST["uid"];
 
-    if($conn->query("UPDATE user SET admin=1 WHERE userid='$id'")) {
-        $_SESSION['admin']=true;
+    if ($conn->query("UPDATE user SET admin=1 WHERE userid='$id'")) {
+        $_SESSION['admin'] = true;
         $_SESSION['verifymsg'] = "<div class='alert alert-success'>
                              <span class='glyphicon glyphicon-info-sign'></span>User account has been assigned admin privilege!
                             </div>";
         header("Location: viewusers.php");
-    }else{
+    } else {
         $_SESSION['verifymsg'] = "<div class='alert alert-success'>
                              <span class='glyphicon glyphicon-info-sign'></span>Failed to assign admin privilege!
                             </div>";
         header("Location: viewusers.php");
     }
+}
 
-    if(isset($_POST['uma'])) {
+if(isset($_POST['uma'])) {
         $uid = $_POST['uid'];
         $user = $conn->query("SELECT * FROM user WHERE userid='$uid'");
         $urow = $user->fetch_assoc();
@@ -40,12 +41,7 @@ if(isset($_POST["make"])) {
         $email=$urow['email'];
 
 
-        $_SESSION['verifymsg'] = "<div class='alert alert-success'>
-            <span class='glyphicon glyphicon-info-sign'></span>Admin privilege has been revoked!
-            </div>";
-        header("Location: viewusers.php");
-
-        /*$checks=$conn->query("UPDATE user SET admin = 0,active = $active,hash = '$hash',username = '$username'
+        $checks=$conn->query("UPDATE user SET admin = 0,active = $active,hash = '$hash',username = '$username'
             ,email= '$email',password = '$password',userid = '$userid'WHERE `userid` = '$userid'");
             if ($checks) {
             $_SESSION['verifymsg'] = "<div class='alert alert-success'>
@@ -57,6 +53,6 @@ if(isset($_POST["make"])) {
             <span class='glyphicon glyphicon-info-sign'></span>User account failed to be revoked!
             </div>";
             header("Location: viewusers.php");
-        }*/
+        }
     }
 }?>
