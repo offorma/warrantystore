6dc2f362337554a54715e2a95dab3b930bfe9de1
Chukwunonso <offorma@gmail.com>
2017-04-24 11:22:17 +0100

    adding form to gallery

--- php/searchresult.php
+++ php/searchresult.php
@@ -49,7 +49,7 @@ if(!isset($_SESSION['userSession'])||$_SESSION['active'] == false){
                             </a>
                             <button class='btn btn-success' data-href='$imageThumbURL' data-toggle='modal' data-target='#ocr'> Recognize
                         <i class='fa'></i></button>
-                    <button class='btn btn-danger' data-href='$imageThumbURL' data-toggle='modal' data-target='#confirm-delete\'> Delete
+                    <button class='btn btn-danger' data-href='$imageThumbURL' data-toggle='modal' data-target='#confirm'> Delete
                     <i class='fa fa-trash-o'></i></button>
                         </div>";
 
@@ -93,7 +93,7 @@ if(!isset($_SESSION['userSession'])||$_SESSION['active'] == false){
                             </a>
                             <button class='btn btn-success' data-href='$imageThumbURL' data-toggle='modal' data-target='#ocr'> Recognize
                         <i class='fa'></i></button>
-                    <button class='btn btn-danger' data-href='$imageThumbURL' data-toggle='modal' data-target='#confirm-delete\'> Delete
+                    <button class='btn btn-danger' data-href='$imageThumbURL' data-toggle='modal' data-target='#confirm'> Delete
                     <i class='fa fa-trash-o'></i></button>
                         </div>";
 
